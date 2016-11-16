85b7b0c16c1eb1ed8944abe1bea64502b3921d48
Piotr Slon <peslonx@gmail.com>
2016-11-16 11:16:29 +0100

    logaut button in footer because im lazy

--- catbaseboot/src/main/java/com/catbaseboot/controllers/LoginController.java
+++ catbaseboot/src/main/java/com/catbaseboot/controllers/LoginController.java
@@ -1,17 +1,39 @@
 package com.catbaseboot.controllers;
 
-import java.text.ParseException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class LoginController {
 	
 	@RequestMapping("/login")
-	public String start (Model model) throws ParseException {
+	public String loginPage () {
 		
 		return "login";
 	}
+	
+	@RequestMapping("/logout")
+	public String logoutPage (HttpServletRequest request, HttpServletResponse response) {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		if (auth != null) {
+			new SecurityContextLogoutHandler().logout(request, response, auth);
+		}
+		
+		return "index";
+	}
+	
+	/*@RequestMapping(value = "/logout", method = RequestMethod.GET)
+	public String logoutPage(HttpServletRequest request, HttpServletResponse response) {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		if (auth != null) {
+			new SecurityContextLogoutHandler().logout(request, response, auth);
+		}
+		return "redirect:/";
+	}*/
 }
