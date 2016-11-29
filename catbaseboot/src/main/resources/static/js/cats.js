b5f532a3fcccedf10804c0b824760687c4ef0008
Piotr Slon <peslonx@gmail.com>
2016-11-29 10:18:37 +0100

    add toggle "add toy"

--- catbaseboot/src/main/resources/static/js/cats.js
+++ catbaseboot/src/main/resources/static/js/cats.js
@@ -3,4 +3,14 @@ $(document).ready(function () {
 	setTimeout(function() {
 		$( ".alert" ).remove();
 	}, 3000);
+	
+	$(".add_toy").click(function() {
+		if ($(".add_toy_form").hasClass( "hide" )) {
+			$(".add_toy_form").removeClass("hide");
+		} else {
+			$(".add_toy_form").toggle();
+		}
+
+    });
+
 });
