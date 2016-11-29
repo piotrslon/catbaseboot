4b38472faab82e04e12e544b996d965157a0bed4
Piotr Slon <peslonx@gmail.com>
2016-11-29 12:32:30 +0100

    create form to add catstoys

--- catbaseboot/src/main/java/com/catbaseboot/controllers/ToysController.java
+++ catbaseboot/src/main/java/com/catbaseboot/controllers/ToysController.java
@@ -4,8 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.catbaseboot.dto.RegisterFormDto;
+import com.catbaseboot.model.CatsToys;
 
 @Controller
 public class ToysController {
@@ -16,6 +17,19 @@ public class ToysController {
 		
 		return "/cats/toys";
 	}*/
+	/*@RequestMapping(value="/cats/toys/add", method = RequestMethod.GET)
+	public String addCats (@ModelAttribute("catsToys") CatsToys catsToys) {
+		
+		return "/cats/show";
+	}*/
+	
+	@RequestMapping(value="/cats/toys/add", method = RequestMethod.POST)
+	public String addToy (@ModelAttribute("catsToys") CatsToys catsToys, RedirectAttributes redirectAttributes) {
+		
+		System.out.println("addToy");
+		
+		return "redirect:/cats/show";
+	}
 	
 		//zabawki w osobnej liscie, i z tej listy dodawane do k?
 		//czy kazdy k. a rozne zabawki dodawane indywidualnie?
